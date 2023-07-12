2225bb6121949218a757236f06e78bd71fd74c5e
Yang-ah <yangah0409@gmail.com>
2023-07-13 00:23:48 +0900

    Design: DetailPage 마크다운 본문

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -7,8 +7,10 @@ import styled from '@emotion/styled';
 const Detail = () => {
   const { detail, isLoading } = useDetail();
 
-  return (
-    <main>
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <Wrap>
       <Header>
         <ImageContainer>
           <img src={detail.user.avatar_url} alt="userImage" />
@@ -23,11 +25,16 @@ const Detail = () => {
           username={detail.user.login}
         />
       </Header>
-      <ReactMarkdown>{detail.body}</ReactMarkdown>
-      {isLoading && <Loading/>}
-    </main>
+      <Main>
+        <ReactMarkdown>{detail.body}</ReactMarkdown>
+      </Main>
+    </Wrap>
   );
 };
+
+const Wrap = styled.div`
+  width: 80%;
+`;
 
 const Header = styled.header`
   display: flex;
@@ -43,6 +50,45 @@ const ImageContainer = styled.div`
   > img {
     width: 100%;
     border-radius: 4px;
+  }
+`;
+
+const Main = styled.section`
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: 40px;
+  border-radius: 0px 40px 40px 40px;
+  border: 0.5px solid #d0d7de;
+  font-weight: 300;
+
+  h1,
+  h2 {
+    width: 100%;
+    padding: 20px 0;
+    margin-bottom: 20px;
+    border-bottom: 0.5px solid #d0d7de;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+
+  code {
+    white-space: break-spaces;
+    width: 100%;
+    background-color: #eee;
+    border-radius: 3px;
+    padding: 0 3px;
+  }
+
+  img {
+    margin: 20px 0;
+    width: 100%;
+    max-width: 600px;
+  }
+
+  li {
+    padding: 8px 0;
   }
 `;
 export default Detail;
