3e47b41792146eaf649f6190f155edf8637e8f79
Yang-ah <yangah0409@gmail.com>
2023-07-12 23:36:20 +0900

    Design: NotFound, Advertisement

--- src/components/Advertisement/index.tsx
+++ src/components/Advertisement/index.tsx
@@ -4,6 +4,7 @@ import { IImage } from '../../models';
 const Advertisement = ({ src, alt, path }: IImage) => {
   return (
     <Link href={path}>
+      <h3>[ AD ]</h3>
       <img src={src} alt={alt} />
     </Link>
   );
@@ -11,9 +12,19 @@ const Advertisement = ({ src, alt, path }: IImage) => {
 
 const Link = styled.a`
   width: 100%;
+  background-color: white;
   display: block;
-  padding: 4px;
+  padding: 8px 12px;
+  border-radius: 4px;
   margin: 20px 0;
+  display: flex;
+  align-items: center;
+
+  > h3 {
+    padding: 0 4px 4px 0;
+    font-size: 16px;
+    color: rgb(51, 51, 51);
+  }
 `;
 
 export default Advertisement;
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,30 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
+const description = `
+잘못된 경로 접근으로 인한 에러입니다. 
+로고를 클릭하여 home으로 이동해주세요 🤗
+`;
+
 const NotFound = () => {
-  return <main>Not Found Page</main>;
+  return (
+    <Main>
+      <h1> 404 Not Found</h1>
+      <p>{description}</p>
+    </Main>
+  );
 };
+
+const Main = styled.main`
+  width: 100%;
+  padding-top: 100px;
+  text-align: center;
+  white-space: pre-wrap;
+  letter-spacing: 1px;
+
+  > p {
+    font-weight: 300;
+  }
+`;
 
 export default NotFound;
